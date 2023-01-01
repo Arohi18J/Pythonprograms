ee13c447666456062bd5244ae8199747aa248f1d
Arohi1Jambenal <103898925+Arohi1Jambenal@users.noreply.github.com>
2023-01-01 22:30:36 +0530

    Add files via upload

---
+++ listsandtuples.py
@@ -0,0 +1,24 @@
+friends=["Arohi","Maanik","Ariya","Simran","Kavish"]
+print(friends[0:3])
+
+A=[2,45,32,77,56,1,23]
+A.sort()
+print(A)
+
+A.reverse()
+print(A)
+
+A.append(91)
+print(A)
+
+A.insert(1,22)
+print(A)
+
+A.pop(4)
+print(A)
+
+B=(1,2,34,35,36,58)
+B1=(2,)
+print(B1)
+
+print(B.count(2)) #to find how many times an element is repeated
